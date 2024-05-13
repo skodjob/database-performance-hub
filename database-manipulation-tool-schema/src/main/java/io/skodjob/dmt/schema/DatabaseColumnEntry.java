56c0434ac2050792413711489c0a1100f21e72ad
Ondrej Babec <ond.babec@gmail.com>
2024-05-13 09:40:13 +0200

    Release misc (#9)

--- database-manipulation-tool-schema/src/main/java/io/skodjob/dmt/schema/DatabaseColumnEntry.java
+++ database-manipulation-tool-schema/src/main/java/io/skodjob/dmt/schema/DatabaseColumnEntry.java
@@ -1,44 +1,118 @@
+/*
+ *
+ * MIT License
+ *
+ * Copyright (c) [2024] [Ondrej Babec <ond.babec@gmail.com>, Jiri Novotny <novotnyjirkajn@gmail.com>]
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY
+ * KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO
+ * EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ * DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE
+ * ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 package io.skodjob.dmt.schema;
 
 import java.util.Objects;
 
+/**
+ * The type Database column entry.
+ */
 public final class DatabaseColumnEntry {
     private String value;
     private String columnName;
     private String dataType;
 
+    /**
+     * Instantiates a new Database column entry.
+     *
+     * @param value      the value
+     * @param columnName the column name
+     * @param dataType   the data type
+     */
     public DatabaseColumnEntry(String value, String columnName, String dataType) {
         this.value = value;
         this.columnName = columnName;
         this.dataType = dataType;
     }
 
+    /**
+     * Instantiates a new Database column entry.
+     */
     public DatabaseColumnEntry() {
         this.value = "UNDEFINED";
         this.columnName = "UNDEFINED";
         this.dataType = "UNDEFINED";
     }
 
+    /**
+     * Gets value.
+     *
+     * @return the value
+     */
     public String getValue() {
         return value;
     }
 
+    /**
+     * Sets value.
+     *
+     * @param value the value
+     */
     public void setValue(String value) {
         this.value = value;
     }
 
+    /**
+     * Gets column name.
+     *
+     * @return the column name
+     */
     public String getColumnName() {
         return columnName;
     }
 
+    /**
+     * Sets column name.
+     *
+     * @param columnName the column name
+     */
     public void setColumnName(String columnName) {
         this.columnName = columnName;
     }
 
+    /**
+     * Gets data type.
+     *
+     * @return the data type
+     */
     public String getDataType() {
         return dataType;
     }
 
+    /**
+     * Sets data type.
+     *
+     * @param dataType the data type
+     */
     public void setDataType(String dataType) {
         this.dataType = dataType;
     }
@@ -69,14 +143,29 @@ public final class DatabaseColumnEntry {
         return Objects.hash(value, columnName, dataType);
     }
 
+    /**
+     * Value string.
+     *
+     * @return the string
+     */
     public String value() {
         return value;
     }
 
+    /**
+     * Column name string.
+     *
+     * @return the string
+     */
     public String columnName() {
         return columnName;
     }
 
+    /**
+     * Data type string.
+     *
+     * @return the string
+     */
     public String dataType() {
         return dataType;
     }
--- database-manipulation-tool-schema/src/main/java/io/skodjob/dmt/schema/DatabaseEntry.java
+++ database-manipulation-tool-schema/src/main/java/io/skodjob/dmt/schema/DatabaseEntry.java
@@ -1,3 +1,34 @@
+/*
+ *
+ * MIT License
+ *
+ * Copyright (c) [2024] [Ondrej Babec <ond.babec@gmail.com>, Jiri Novotny <novotnyjirkajn@gmail.com>]
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY
+ * KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO
+ * EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ * DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE
+ * ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 package io.skodjob.dmt.schema;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
@@ -8,60 +39,116 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
+/**
+ * The type Database entry.
+ */
 public class DatabaseEntry {
 
     private List<DatabaseColumnEntry> columnEntries;
     private String name;
     private String primary;
 
+    /**
+     * Instantiates a new Database entry.
+     *
+     * @param columnEntries the column entries
+     * @param name          the name
+     * @param primary       the primary
+     */
     public DatabaseEntry(List<DatabaseColumnEntry> columnEntries, String name, String primary) {
         this.columnEntries = columnEntries;
         this.name = name;
         this.primary = primary;
     }
 
+    /**
+     * Instantiates a new Database entry.
+     *
+     * @param name    the name
+     * @param primary the primary
+     */
     public DatabaseEntry(String name, String primary) {
         this.name = name;
         this.primary = primary;
         columnEntries = new ArrayList<>();
     }
 
+    /**
+     * Instantiates a new Database entry.
+     */
     public DatabaseEntry() {
         this.name = "UNDEFINED";
         this.primary = "UNDEFINED";
         columnEntries = new ArrayList<>();
     }
 
+    /**
+     * Gets column entries.
+     *
+     * @return the column entries
+     */
     public List<DatabaseColumnEntry> getColumnEntries() {
         return columnEntries;
     }
 
+    /**
+     * Sets column entries.
+     *
+     * @param columnEntries the column entries
+     */
     public void setColumnEntries(List<DatabaseColumnEntry> columnEntries) {
         this.columnEntries = columnEntries;
     }
 
+    /**
+     * Gets name.
+     *
+     * @return the name
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Sets name.
+     *
+     * @param name the name
+     */
     public void setName(String name) {
         this.name = name;
     }
 
+    /**
+     * Gets primary.
+     *
+     * @return the primary
+     */
     public String getPrimary() {
         return primary;
     }
 
+    /**
+     * Sets primary.
+     *
+     * @param primary the primary
+     */
     public void setPrimary(String primary) {
         this.primary = primary;
     }
 
+    /**
+     * Add column entry.
+     *
+     * @param columnEntry the column entry
+     */
     public void addColumnEntry(DatabaseColumnEntry columnEntry) {
         columnEntries.add(columnEntry);
     }
 
 
     /**
+     * Gets primary column entry.
+     *
      * @return DatabaseColumnEntry which is the primary in this DatabaseEntry
      */
     @JsonIgnore
@@ -101,6 +188,12 @@ public class DatabaseEntry {
         return Objects.hash(columnEntries);
     }
 
+    /**
+     * To json string string.
+     *
+     * @return the string
+     * @throws JsonProcessingException the json processing exception
+     */
     public String toJsonString() throws JsonProcessingException {
         return new ObjectMapper().writeValueAsString(this);
     }

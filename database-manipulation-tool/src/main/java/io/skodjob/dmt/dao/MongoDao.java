2aaa8c6c34d3d4d40eb47188cde912e013b944f8
Jiri Novotny <jnovotny@redhat.com>
2024-05-30 09:42:48 +0200

    remove unused function in MongoDao

--- database-manipulation-tool/src/main/java/io/skodjob/dmt/dao/MongoDao.java
+++ database-manipulation-tool/src/main/java/io/skodjob/dmt/dao/MongoDao.java
@@ -185,10 +185,6 @@ public final class MongoDao implements Dao {
     }
 
     @Override
-    public void executePreparedStatement(String statement) {
-    }
-
-    @Override
     public void executeBatchStatement(List<String> statements) {
     }
 
